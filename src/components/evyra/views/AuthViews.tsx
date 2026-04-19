baa37193fb0e200b643afaee773ff16e35808f62
Adrian <netlinkassist@gmail.com>
2026-04-19 17:48:14 -0300

    test: embed content directly

--- src/components/evyra/views/AuthViews.tsx
+++ src/components/evyra/views/AuthViews.tsx
@@ -1,2 +1,8 @@
-// TEST DIRECT COMMIT
-const test = true;
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+export const TestView = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return <div>Test</div>;
+};
